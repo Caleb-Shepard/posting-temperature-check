bdbc16b430699368dcadfcebf04492dee67c74de
bernstagon <bren.gmp7@yahoo.com>
2018-01-20 12:15:18 -0500

    deleted stuff

---
+++ sort.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+f = open("textfile", "r")
+
+# print first word per line
+while True:
+    l = f.readline()
+    if l == "":
+        break
+    
+    
+    firstword = l.split(" ")[0].strip()
+    
+    if firstword.isalpha():
+        print(firstword)
+
+    print("*************")
